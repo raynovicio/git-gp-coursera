a3f41ee57f3c09e2acc092bd971819890c81cf25
raynovicio <ray.novicio@hq.doe.gov>
2024-10-15 14:56:08 +0000

    feat(src/app.py): added the update item to route API

--- src/app.py
+++ src/app.py
@@ -3,12 +3,28 @@ from fastapi import FastAPI
 from models import Item
 app = FastAPI()
 app.add_event_handler("startup", startup_event)
-@app.post("/items/")
-def create_item(item:Item):
+# Update items API route
+@app.put("/items/{item_id}")
+def update_item(item_id:int, item:Item):
+    """API route to update an item from database trought the item id.
+
+    Parameters
+    ----------
+    item_id : int
+        Item unique identification
+    item : Item
+        The item itself following the Item model
+
+    Returns
+    -------
+    Item
+        The item after it has been added to database
+    """
     conn = get_db()
-    cursor = conn.cursor()
-    cursor.execute(
-        "INSERT INTO items (name, price, is_offer) VALUES (?, ?, ?)", (item.name, item.price, int(item.is_offer) if item.is_offer else None), )
+    conn.execute(
+        "UPDATE items SET name = ?, price = ?, is_offer = ? WHERE id = ?",
+        (item.name, item.price, int(item.is_offer) 
+        if item.is_offer 
+        else None, item_id),)
     conn.commit()
-    item.id = cursor.lastrowid
     return item
